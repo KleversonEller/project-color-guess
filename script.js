f3289f677617129e8e9ef379fb3ad0b56b07e59c
Kleverson Eller <kleverson.a16@gmail.com>
2021-12-23 00:12:58 -0300

    Requisito 2 e 5

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ window.onload = function () {
     for (let index = 1; index <= 6; index += 1) {
       let criaCor = document.createElement('li');
       criaCor.classList.add('ball');
-      criaCor.style.backgroundColor = geraNumero(1, 255);
+      criaCor.style.backgroundColor = geraNumero(1, 256);
       listaDeCores.appendChild(criaCor);
     }
   }
@@ -27,4 +27,41 @@ window.onload = function () {
   criaCores();
 
   //! Cria 6 círculos com uma cor dentro
+
+  //? Requisito 2 - 5
+
+  function geraNumero2(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+  let corAleatoria = geraNumero2(1, 7) - 1;
+  let paragrafo = document.querySelector('#rgb-color');
+  function escreveCorAleatoria() {
+    let listaCores = [];
+    let cores = document.querySelectorAll('.ball');
+
+    for (let posicao of cores) {
+      let cor = posicao.style.backgroundColor;
+      listaCores.push(cor);
+    }
+    paragrafo.innerHTML = listaCores[corAleatoria];
+  }
+
+  escreveCorAleatoria();
+
+  //! Preenche os círculos com cores aleatórias e escreve uma delas como texto do paragrafo
+
+  let escolherCor = document.querySelector('#lista-cores');
+  escolherCor.addEventListener('click', function (evento) {
+    if (evento.target.classList.value == 'ball') {
+      if (evento.target.style.backgroundColor === paragrafo.innerHTML) {
+        document.querySelector('#answer').innerHTML = 'Acertou!';
+      } else {
+        document.querySelector('#answer').innerHTML = 'Errou! Tente novamente!';
+      }
+    }
+  });
+  //! Verifica se a cor clicada é a mesma que aparece no testo
 };
